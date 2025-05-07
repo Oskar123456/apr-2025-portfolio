541a690e6e4df90986bb13d5d0610d4419b18bbc
oskar123456 <cph-oh82@cphbusiness.dk>
2025-05-07 19:31:34 +0200

    gui example

---
+++ src/main/java/apr/GUIExample.java
@@ -0,0 +1,15 @@
+package apr;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * GUIExample
+ */
+public abstract class GUIExample extends Pane {
+
+    public abstract void start();
+
+    public abstract void pause();
+
+    public abstract void stop();
+}
---
+++ src/main/java/apr/algorithms/graph/visualization/AStarGuiExample.java
@@ -0,0 +1,8 @@
+package apr.algorithms.graph.visualization;
+
+/**
+ * AStarGuiExample
+ */
+public class AStarGuiExample extends GUIExample {
+
+}
