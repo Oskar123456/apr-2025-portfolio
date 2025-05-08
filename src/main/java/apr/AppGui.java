6a864e8847df278f7e665637061108ad0babc78e
oskar123456 <cph-oh82@cphbusiness.dk>
2025-05-08 05:14:09 +0200

    .

--- src/main/java/apr/AppGui.java
+++ src/main/java/apr/AppGui.java
@@ -43,30 +43,7 @@ public class AppGui extends Application {
 
         content = new AStarGuiExample(W - 2 * padding, H - 2 * padding);
 
-        topPanel = new HBox();
-        bottomPanel = new HBox();
-
-        titleTxt = new Text("APR 2025 PORTFOLIO GUI EXAMPLES");
-        titleTxt.setStyle("-fx-font-size: 20px");
-        topPanel.getChildren().add(titleTxt);
-        footnoteTxt = new Text("FOOTNOTES / INFO");
-        footnoteTxt.setStyle("-fx-font-size: 10px");
-        bottomPanel.getChildren().add(footnoteTxt);
-
-        sidePanel = new VBox();
-        sidePanel.setMinWidth(100);
-        Text sidePanelTitle = new Text("Side Panel");
-        sidePanelTitle.setStyle("-fx-font-size: 15px");
-        sidePanel.getChildren().add(sidePanelTitle);
-
-        BorderPane layout = new BorderPane();
-
-        layout.setTop(topPanel);
-        layout.setBottom(bottomPanel);
-        layout.setCenter(content);
-        layout.setLeft(sidePanel);
-
-        root.getChildren().add(layout);
+        root.getChildren().add(content);
 
         content.start();
 
@@ -74,6 +51,51 @@ public class AppGui extends Application {
         stage.show();
     }
 
+    //
+    // @Override
+    // public void start(Stage stage) {
+    // // var javaVersion = SystemInfo.javaVersion();
+    // // var javafxVersion = SystemInfo.javafxVersion();
+    //
+    // int W = 1000, H = 1000;
+    // double padding = 10;
+    //
+    // root = new Group();
+    // Scene s = new Scene(root, W, H, Color.WHITE);
+    //
+    // content = new AStarGuiExample(W - 2 * padding, H - 2 * padding);
+    //
+    // topPanel = new HBox();
+    // bottomPanel = new HBox();
+    //
+    // titleTxt = new Text("APR 2025 PORTFOLIO GUI EXAMPLES");
+    // titleTxt.setStyle("-fx-font-size: 20px");
+    // topPanel.getChildren().add(titleTxt);
+    // footnoteTxt = new Text("FOOTNOTES / INFO");
+    // footnoteTxt.setStyle("-fx-font-size: 10px");
+    // bottomPanel.getChildren().add(footnoteTxt);
+    //
+    // sidePanel = new VBox();
+    // sidePanel.setMinWidth(100);
+    // Text sidePanelTitle = new Text("Side Panel");
+    // sidePanelTitle.setStyle("-fx-font-size: 15px");
+    // sidePanel.getChildren().add(sidePanelTitle);
+    //
+    // BorderPane layout = new BorderPane();
+    //
+    // layout.setTop(topPanel);
+    // layout.setBottom(bottomPanel);
+    // layout.setCenter(content);
+    // layout.setLeft(sidePanel);
+    //
+    // root.getChildren().add(layout);
+    //
+    // content.start();
+    //
+    // stage.setScene(s);
+    // stage.show();
+    // }
+    //
     public static void main(String[] args) throws IOException {
         launch();
     }
